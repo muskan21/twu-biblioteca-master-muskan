0793f39274bdf66bdefe5aab12a2b6da9c7362b4
muskan21 <muskan.cs.du.2013@gmail.com>
2015-09-05 10:57:50 +0000

    Created BookList Class And Specs To represnt a list of books and to display it

--- src/com/twu/biblioteca/BookList.java
+++ src/com/twu/biblioteca/BookList.java
@@ -1,7 +1,22 @@
 package com.twu.biblioteca;
 
-/**
- * Created by muskandhanda on 9/4/15.
- */
+import java.util.ArrayList;
+
 public class BookList {
+    private ArrayList<Book> bookList;
+
+    public BookList(ArrayList<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    public void display() {
+        System.out.printf("%-30s %-30s %-30s\n\n", "Book Name", "Book Author", "Year Of Publish");
+        for(Book book : bookList) {
+            book.display();
+        }
+    }
+
+    public int size() {
+        return bookList.size();
+    }
 }
--- test/com/twu/biblioteca/BookListTest.java
+++ test/com/twu/biblioteca/BookListTest.java
@@ -1,10 +1,35 @@
 package com.twu.biblioteca;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
-/**
- * Created by muskandhanda on 9/4/15.
- */
-public class BookListTest extends TestCase {
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
 
+import static org.junit.Assert.assertEquals;
+
+public class BookListTest {
+
+    @Test
+    public void initializeBooksShouldAddTheBooksToTheList() {
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(new Book("Gone girl","Gyllian Flynn", 2012));
+        BookList bookList = new BookList(books);
+
+        assertEquals(1, bookList.size());
+    }
+
+    @Test
+    public void shouldPrintTheListOfBooksOnConsole() {
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(new Book("Gone Girl","Gillian Flynn", 2012));
+        BookList bookList = new BookList(books);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        bookList.display();
+
+        assertEquals("Gone Girl\t\tGillian Flynn\t\t2012\n", out.toString());
+        System.setOut(System.out);
+    }
 }
