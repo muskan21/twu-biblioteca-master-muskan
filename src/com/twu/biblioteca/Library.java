a02bd9be12f6eebb0e060882bfdd14fc6c1a1add
muskan21 <muskan.cs.du.2013@gmail.com>
2015-09-08 06:24:38 +0000

    Added functionality for check out which needed overriding equals and hashcode

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -13,6 +13,11 @@ public class Library {
         this.checkedOutBookList = new ArrayList<Book>();
     }
 
+    public Library(ArrayList<Book> availableBookList, ArrayList<Book> checkedOutBookList) {
+        this.availableBookList = availableBookList;
+        this.checkedOutBookList = checkedOutBookList;
+    }
+
     public void display() {
         System.out.printf("%-30s %-30s %-30s\n\n","Book Name", "Book Author", "Year Of Publish");
         for(Book book : availableBookList) {
@@ -24,8 +29,10 @@ public class Library {
         return availableBookList.size();
     }
 
-    public void CheckOutBook() {
-        return;
+    public boolean CheckOutBook() {
+        String checkedOutBook = inputCheckedOutBook();
+        boolean available = checkAvailabilityForCheckOut(checkedOutBook);
+        return available;
     }
 
     public String inputCheckedOutBook() {
@@ -38,9 +45,49 @@ public class Library {
     public boolean checkAvailabilityForCheckOut(String bookName) {
         Book checkOutBook = new Book(bookName);
         for(Book book : availableBookList) {
-            if(book.equals(checkOutBook))
+            if(book.equals(checkOutBook)) {
+                availableBookList.remove(book);
+                checkedOutBookList.add(book);
                 return true;
+            }
         }
         return false;
+    }
+
+    @Override
+    public boolean equals(Object bookObject) {
+        if (this == bookObject) return true;
+        if (bookObject == null || getClass() != bookObject.getClass()) return false;
+
+        Library library = (Library) bookObject;
+        int flag = 0;
+        for(Book book: availableBookList) {
+            flag = 0;
+            for (Book book1 : library.availableBookList)
+                if (book.equals(book1))
+                    flag = 1;
+            if (flag != 1)
+                return false;
+        }
+        for(Book book: checkedOutBookList) {
+            flag = 0;
+            for (Book book1 : library.checkedOutBookList)
+                if (book.equals(book1))
+                    flag = 1;
+            if (flag != 1)
+                return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 0;
+        for (Book book: availableBookList)
+            result = result + book.hashCode();
+        for(Book book: checkedOutBookList)
+            result = result + book.hashCode();
+        result = 31 * result;
+        return result;
     }
 }
--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -7,9 +7,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotEquals;
 
 public class LibraryTest {
 
@@ -69,5 +68,136 @@ public class LibraryTest {
         boolean available = library.checkAvailabilityForCheckOut("Harry potter");
 
         assertFalse(available);
+    }
+
+    @Test
+    public void listOfAvailableBooksShouldNotHaveTheCheckedOutBook() {
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(new Book("Gone Girl", "Gillian Flynn", 2012));
+        books.add(new Book("Harry Potter", "J.K. Rowling", 2000));
+        Library library = new Library(books);
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream("Harry Potter".getBytes());
+        System.setIn(byteArrayInputStream);
+
+        boolean checkOutFlag = library.CheckOutBook();
+
+        ArrayList<Book> books1 = new ArrayList<Book>();
+        books1.add(new Book("Gone Girl", "Gillian Flynn", 2012));
+        ArrayList<Book> books2 = new ArrayList<Book>();
+        books2.add(new Book("Harry Potter", "J.K. Rowling", 2000));
+        Library library1 = new Library(books1, books2);
+
+        assertEquals(library, library1);
+    }
+
+    @Test
+    public void shouldBeEqualWhenComparingABookListToItself() {
+        Book Book = new Book("Christmas Carol", "Charles Dickens", 1843);
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(new Book("Gone Girl", "Gillian Flynn", 2012));
+        books.add(new Book("Harry Potter", "J.K. Rowling", 2000));
+        books.add(Book);
+        Library library = new Library(books);
+        assertEquals(library, library);
+    }
+
+    @Test
+    public void shouldBeEqualWhenComparingABookListToAnotherBookListWithSameBooks() {
+        Book Book = new Book("Christmas Carol", "Charles Dickens", 1843);
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(Book);
+        Library library = new Library(books);
+        Book Book1 = new Book("Christmas Carol", "Charles", 1843);
+        ArrayList<Book> books1 = new ArrayList<Book>();
+        books1.add(Book1);
+        Library library1 = new Library(books1);
+        assertEquals(library, library1);
+    }
+
+    @Test
+    public void shouldBeEqualWhenComparingABookListToAnotherBookListWithSameNameBooksIgnoringTheCase() {
+        Book Book = new Book("Christmas Carol", "Charles Dickens", 1843);
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(Book);
+        Library library = new Library(books);
+        Book Book1 = new Book("christmas carol", "Charles", 1843);
+        ArrayList<Book> books1 = new ArrayList<Book>();
+        books1.add(Book1);
+        Library library1 = new Library(books1);
+        assertEquals(library, library1);
+    }
+
+    @Test
+    public void shouldNotBeEqualWhenComparingABookListToNull() {
+        Book Book = new Book("Christmas Carol", "Charles Dickens", 1843);
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(Book);
+        Library library = new Library(books);
+        assertNotEquals(library, null);
+    }
+
+    @Test
+    public void shouldNotBeEqualWhenComparingABookListToNonBookListEntity() {
+        Book Book = new Book("Christmas Carol", "Charles Dickens", 1843);
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(Book);
+        Library library = new Library(books);
+        assertNotEquals(library, "I am Not a Book");
+    }
+
+    @Test
+    public void shouldNotBeEqualWhenComparingABookListToAnotherBookListWithDifferentNameBooks() {
+        Book book = new Book("Christmas Carol", "Charles Dickens", 1843);
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(book);
+        Library library = new Library(books);
+        Book book1 = new Book("Christmas Carol Bleh", "Charles Dickens", 1843);
+        ArrayList<Book> books1 = new ArrayList<Book>();
+        books1.add(book1);
+        Library library1 = new Library(books1);
+        assertNotEquals(library, library1);
+    }
+
+    @Test
+    public void shouldHaveSameHashCodeComparingABookListToItself() {
+        Book Book = new Book("Christmas Carol", "Charles Dickens", 1843);
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(Book);
+        Library library = new Library(books);
+        assertEquals(library.hashCode(), library.hashCode());
+    }
+
+    @Test
+    public void shouldHaveSameHashCodeComparingABookListToAnotherBookListWithSameNameBooks() {
+        Book book = new Book("Christmas Carol", "Charles Dickens", 1843);
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(book);
+        books.add(new Book("Gone Girl", "Gillian Flynn", 2012));
+        books.add(new Book("Harry Potter", "J.K. Rowling", 2000));
+        Library library = new Library(books);
+        Book book1 = new Book("Christmas Carol", "Charles Dickens", 1843);
+        ArrayList<Book> books1 = new ArrayList<Book>();
+        books1.add(book1);
+        books1.add(new Book("Gone Girl", "Gillian Flynn", 2012));
+        books1.add(new Book("Harry Potter", "J.K. Rowling", 2000));
+        Library library1 = new Library(books1);
+        assertEquals(library.hashCode(), library1.hashCode());
+    }
+
+    @Test
+    public void shouldHaveSameHashCodeWhenComparingABookListToAnotherBookListWithSameNameBooksIgnoringTheCase() {
+        Book book = new Book("Christmas Carol", "Charles Dickens", 1843);
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(book);
+        books.add(new Book("Gone Girl", "Gillian Flynn", 2012));
+        books.add(new Book("Harry Potter", "J.K. Rowling", 2000));
+        Library library = new Library(books);
+        Book book1 = new Book("christmas carol", "Charles", 1843);
+        ArrayList<Book> books1 = new ArrayList<Book>();
+        books1.add(book1);
+        books1.add(new Book("gone Girl", "Gillian Flynn", 2012));
+        books1.add(new Book("harry Potter", "J.K. Rowling", 2000));
+        Library library1 = new Library(books1);
+        assertEquals(library.hashCode(), library1.hashCode());
     }
 }
