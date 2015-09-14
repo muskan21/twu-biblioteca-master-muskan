2bc458210a3d6148d7d99249e0b9e03714b51a0a
muskan21 <muskan.cs.du.2013@gmail.com>
2015-09-14 11:56:06 +0000

    Added spec and functionality for producing a printable formatted details of a movie

--- src/com/twu/biblioteca/Movie.java
+++ src/com/twu/biblioteca/Movie.java
@@ -1,7 +1,19 @@
 package com.twu.biblioteca;
 
-/**
- * Created by muskandhanda on 9/14/15.
- */
 public class Movie {
+    private String name;
+    private int year;
+    private String director;
+    private String rating;
+
+    public Movie(String name, int year, String director, String rating) {
+        this.name = name;
+        this.year = year;
+        this.director = director;
+        this.rating = rating;
+    }
+
+    public String formattedMovieDetails() {
+        return String.format("%-30s %-30d %-30s %-30s", name, year, director, rating);
+    }
 }
--- test/com/twu/biblioteca/MovieTest.java
+++ test/com/twu/biblioteca/MovieTest.java
@@ -1,7 +1,18 @@
 package com.twu.biblioteca;
 
-/**
- * Created by muskandhanda on 9/14/15.
- */
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MovieTest {
+
+    @Test
+    public void shouldReturnTheFormattedMovieDetails() {
+        Movie movie = new Movie("Muskan", 1234, "Woohhoo", "10");
+
+        String movieDetails = movie.formattedMovieDetails();
+
+        String testString = String.format("%-30s %-30d %-30s %-30s", "Muskan", 1234, "Woohhoo", "10");
+        assertEquals(testString, movieDetails);
+    }
 }
