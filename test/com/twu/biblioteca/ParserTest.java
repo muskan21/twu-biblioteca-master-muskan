0f353e8afdd446703f23728d36fdff749e352f28
muskan21 <muskan.cs.du/2013@gmail.com>
2015-09-30 15:17:22 +0530

    Parser return only list books option object and no other object

--- test/com/twu/biblioteca/ParserTest.java
+++ test/com/twu/biblioteca/ParserTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class ParserTest {
 
@@ -19,5 +20,18 @@ public class ParserTest {
         MenuOptions menuOptions = parser.parse("1");
 
         assertEquals(menuOptions.getClass(), ListBooksOption.class);
+    }
+
+    @Test
+    public void shouldNotReturnAnyOtherObjectExceptListBooksOptionObject() {
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book("Muskan", "Author", 123));
+        books.add(new Book("Muskan Dhanda", "Author Same", 1234));
+        BookLibrary bookLibrary = new BookLibrary(books);
+        Parser parser = new Parser(bookLibrary);
+
+        MenuOptions menuOptions = parser.parse("1");
+
+        assertNotEquals(menuOptions.getClass(), Parser.class);
     }
 }
